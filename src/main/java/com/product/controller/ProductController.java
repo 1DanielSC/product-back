6bd82a6c319d59e94c697a3e43fec7a1e2bcf0fc
1danielsc <danielscolao@gmail.com>
2022-12-03 08:56:19 -0300

    feat: loadbalancer

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.product.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +23,14 @@ public class ProductController {
     
     @Autowired
     private ProductService productService;
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping(value = "/sayHi")
+    public ResponseEntity<String> sayHi(){
+        return ResponseEntity.ok("Hi from + " + port);
+    }
 
     @GetMapping(value = "/findAll")
     public ResponseEntity<List<Product>> findAllProducts(){
